3ebce215364d1872f83fc6818be265cdd7b70dca
Wroah <djnonsens@hotmail.com>
2014-12-11 19:51:07 +0100

    Fixed date parsing RGPO
    Probably...

--- MobowskiSports/RGPOResultParser.cs
+++ MobowskiSports/RGPOResultParser.cs
@@ -30,7 +30,7 @@ namespace Mobowski.Core.Sports
 
 				testNode = node.SelectSingleNode("datum");
 				if (testNode.InnerText != null && testNode.InnerText.Length > 0) {
-					result.Date = testNode.InnerText.ToDate("dd-MMM");
+					result.Date = testNode.InnerText.ToDate("d-MMM");
 				}
 			} catch (Exception ex) {
 				throw new Exception ("failed to parse RGPO result", ex);		
